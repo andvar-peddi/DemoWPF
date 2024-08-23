c5ab474063a2ebed6c21c49c004779b6a4ebc7f9
AndreiAmbo <andrei.varga@ambo.ro>
2024-08-23 09:23:54 +0300

    Scroll bar was fixed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,14 +2,28 @@
 
 namespace DemoWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly double minWidth = 1200;
+        private readonly double minHeight = 1000;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                gridMainScrollViewer.MinWidth = 0;
+                gridMainScrollViewer.MinHeight = 0;
+            }
+            else
+            {
+                gridMainScrollViewer.MinWidth = minWidth;
+                gridMainScrollViewer.MinHeight= minHeight;
+            }
         }
     }
 }
